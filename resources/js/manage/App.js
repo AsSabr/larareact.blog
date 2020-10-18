c14bce1fe36c63af8ce4cd9dcc0ae9ff7c13a274
AsSabr <assabr.org@gmail.com>
2020-10-18 20:52:23 +0200

    Redux install and Categories CRUD

--- resources/js/manage/App.js
+++ resources/js/manage/App.js
@@ -1,23 +1,34 @@
-import React, {Component} from 'react';
+// React
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router, Link, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Link, Route, Switch } from "react-router-dom";
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import rootReducer from "./store/reducers/RootReducer";
 
+// Partials
 import Header from "./components/partials/Header";
 import Sidebar from "./components/partials/Sidebar";
 import Footer from "./components/partials/Footer";
 import Routes from "./Routes";
 
-export default class App extends Component {
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+class App extends Component {
     render() {
         return (
-            <Router>
-                <div className="wrapper">
-                    <Header/>
-                    <Sidebar/>
-                    <Routes/>
-                    <Footer/>
-                </div>
-            </Router>
+            <Provider store={store}>
+                <Router>
+                    <div className="wrapper">
+                        <Header/>
+                        <Sidebar/>
+                        <Routes/>
+                        <Footer/>
+                    </div>
+                </Router>
+            </Provider>
         );
     }
 }
@@ -25,5 +36,5 @@ export default class App extends Component {
 // export default App;
 
 if (document.getElementById('app')) {
-    ReactDOM.render(<App/>, document.getElementById('app'));
+    ReactDOM.render(<App />, document.getElementById('app'));
 }
